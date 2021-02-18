25acb83ce18dd5dda448d852763f06b51aab1eae
Muhammad-Jibril <jbashir52@gmail.com>
2021-02-18 19:20:38 +0800

    frame_system re

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ use sp_runtime::{
 };
 use sp_std::collections::vec_
 deque::VecDeque;
-use system::{ensure_signed, pallet_prelude::*};
+use frame_system::{self as system, ensure_signed, pallet_prelude::*};
 
 #[cfg(test)]
 mod tests;
