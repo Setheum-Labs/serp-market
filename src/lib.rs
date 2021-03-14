44da4f2cdc69209634d85e98017b6c5d78642037
Muhammad-Jibril <jbashir52@gmail.com>
2021-03-15 00:08:15 +0800

    put back serp_market traits

--- src/lib.rs
+++ src/lib.rs
@@ -146,184 +146,184 @@ impl<T: Config> SerpMarket<T::AccountId> for Pallet<T> {
 	type Balance = BalanceOf<T>;
 
 	/// A trait to provide relative `base_price` of `base_settcurrency_id`. 
-		/// The settcurrency `Price` is `base_price * base_unit`.
-		/// For example The `Price` of `JUSD` is `base_price: Price = $1.1 * base_unit: BaseUnit = 1_100`.
-		/// Therefore, the `Price` is got by checking how much `base_currency_peg` can buy `base_unit`, 
-		/// in our example, `1_100` in `base_currency_peg: USD` of `JUSD` can buy `base_unit` of `JUSD` in `USD`.
-		#[pallet::weight(0)]
-		fn get_stable_price(
-			base_settcurrency_id: T::CurrencyId,
-			base_price: PriceUnit,
-		) -> DispatchResultWithPostInfo {
-			type Fix = FixedU128<U64>;
-			let base_unit = T::GetBaseUnit;
-			let amount_of_peg_to_buy_base_currency = Fix::from_num(base_price) * Fix::from_num(base_unit);
-			Price::put(amount_of_peg_to_buy_base_currency); /// the amount of peg that can buy base currency.
-			native::info!("The price of: {} is {} in its peg currency.", base_settcurrency_id, amount_of_peg_to_buy_base_currency);
-			Self::deposit_event(RawEvent::NewPrice(base_settcurrency_id, amount_of_peg_to_buy_base_currency));
-			Ok(())
-		}
-		
-		/// A trait to provide relative price for two currencies. 
-		/// For example, the relative price of `DNAR-JUSD` is `$1_000 / $1.1 = JUSD 1_100`,
-		/// meaning the price compared in `USD` as the peg of `JUSD` for example.. or,
-		/// the relative price of `DNAR-JUSD` is `DNAR 1 / JUSD 0.001 = JUSD 1_000`,
-		/// meaning `DNAR 1` can buy `JUSD 1_000` and therefore `1 DNAR = 0.001 JUSD`.
-		/// But tyhe former is preffered and thus used.
-		#[pallet::weight(0)]
-		fn get_relative_price(
-			base_currency_id: CurrencyIdOf<T>, 
+	/// The settcurrency `Price` is `base_price * base_unit`.
+	/// For example The `Price` of `JUSD` is `base_price: Price = $1.1 * base_unit: BaseUnit = 1_100`.
+	/// Therefore, the `Price` is got by checking how much `base_currency_peg` can buy `base_unit`, 
+	/// in our example, `1_100` in `base_currency_peg: USD` of `JUSD` can buy `base_unit` of `JUSD` in `USD`.
+	#[pallet::weight(0)]
+	fn get_stable_price(
+		base_settcurrency_id: T::CurrencyId,
+		base_price: PriceUnit,
+	) -> DispatchResultWithPostInfo {
+		type Fix = FixedU128<U64>;
+		let base_unit = T::GetBaseUnit;
+		let amount_of_peg_to_buy_base_currency = Fix::from_num(base_price) * Fix::from_num(base_unit);
+		Price::put(amount_of_peg_to_buy_base_currency); /// the amount of peg that can buy base currency.
+		native::info!("The price of: {} is {} in its peg currency.", base_settcurrency_id, amount_of_peg_to_buy_base_currency);
+		Self::deposit_event(RawEvent::NewPrice(base_settcurrency_id, amount_of_peg_to_buy_base_currency));
+		Ok(())
+	}
+	
+	/// A trait to provide relative price for two currencies. 
+	/// For example, the relative price of `DNAR-JUSD` is `$1_000 / $1.1 = JUSD 1_100`,
+	/// meaning the price compared in `USD` as the peg of `JUSD` for example.. or,
+	/// the relative price of `DNAR-JUSD` is `DNAR 1 / JUSD 0.001 = JUSD 1_000`,
+	/// meaning `DNAR 1` can buy `JUSD 1_000` and therefore `1 DNAR = 0.001 JUSD`.
+	/// But tyhe former is preffered and thus used.
+	#[pallet::weight(0)]
+	fn get_relative_price(
+		base_currency_id: CurrencyIdOf<T>, 
+		base_price: PriceUnit, 
+		quote_currency_id: CurrencyIdOf<T>, 
+		quote_price: PriceUnit,
+	) -> DispatchResult {
+		type Fix = FixedU128<U64>;
+		let amount_of_quote_to_buy_base = Fix::from_num(base_price) / Fix::from_num(quote_price);
+		let amount_of_base_to_buy_quote = Fix::from_num(quote_price) / Fix::from_num(base_price);
+		Price::put(base_currency_id, amount_of_quote_to_buy_base); /// the amount of quote currency that can buy base currency.
+		native::info!(
+			"The price of: {} for: {}  is {}, therefore {} {} can buy {} {}", 
+			base_currency_id, 
+			quote_currency_id, 
+			amount_of_quote_to_buy_base, 
+			quote_currency_id, 
+			amount_of_quote_to_buy_base, 
+			base_currency_id, 
+			amount_of_base_to_buy_quote,
+		);
+		Ok(())
+	}
+
+	/// Quote the amount of currency price quoted as serping fee (serp quoting) for Serpers, 
+	/// the Serp Quote is `price/base_unit = fraction`, `fraction - 1 = fractioned`, `fractioned * serp_quote_multiple = quotation`,
+	/// `quotation + fraction = quoted` and `quoted` is the price the SERP will pay for serping in full including the serp_quote,
+	///  the fraction is same as `(market_price + (mint_rate * 2))` - where `market-price = price/base_unit`, 
+	/// `mint_rate = serp_quote_multiple`, and with `(price/base_unit) - 1 = price_change`.
+	///
+	/// Calculate the amount of currency price for SerpMarket's SerpQuote from a fraction given as `numerator` and `denominator`.
+	#[pallet::weight(0)]
+	fn quote_serp_price(price: PriceUnit) -> PriceUnit {
+		type Fix = FixedU128<U64>;
+		let base_unit = T::GetBaseUnit;
+		/// The `serp_quote_multiple` was in place as "[`let serp_quote_multiple = 2;` // (mint_rate * 2) for Serp Quote]".
+		let serp_quote_multiple = T::GetSerpQuoteMultiple;
+		let fraction = Fix::from_num(price) / Fix::from_num(base_unit);
+		let fractioned = Fix::from_num(fraction) - Fix::from_num(1);
+		let quotation = fractioned.saturating_mul_int(serp_quote_multiple as u128).to_num::<u64>();
+		quoted = Fix::from_num(fraction) + Fix::from_num(quotation);
+		Ok(())
+	}
+
+	/// Calculate the amount of supply change from a fraction given as `numerator` and `denominator`.
+	#[pallet::weight(0)]
+	fn calculate_supply_change(new_price: PriceUnit) -> PriceUnit {
+		type Fix = FixedU128<U64>;
+		let base_unit = T::GetBaseUnit; 
+		let supply = T::SettCurrency::total_issuance();
+		let fraction = Fix::from_num(new_price) / Fix::from_num(base_unit) - Fix::from_num(1);
+		fraction.saturating_mul_int(supply as u128).to_num::<u64>();
+		Ok(())
+	}
+
+	/// Called when `expand_supply` is received from the SERP.
+	/// Implementation should `deposit` the `amount` to `serpup_to`, 
+	/// then `amount` will be slashed from `serpup_from` and update
+	/// `new_supply`.
+	#[pallet::weight(0)]
+	fn expand_supply(
+		currency_id: Self::CurrencyId, 
+		expand_by: Self::Balance,
+	) -> DispatchResult{
+		let supply = T::SettCurrency::total_issuance();
+		// Checking whether the supply will overflow.
+		supply
+			.checked_add(expand_by)
+			.ok_or(Error::<T>::SupplyOverflow)?;
+		// ↑ verify ↑
+		let native_asset_id = T::GetStp258NativeId;
+		let serper = &T::GetSerperAcc; 
+		let settpay = &T::GetSettPayAcc;
+		let base_currency_id = currency_id;
+		let quote_currency_id = native_asset_id;
+		let price = Self::get_relative_price(
+			native_asset_id,
 			base_price: PriceUnit, 
-			quote_currency_id: CurrencyIdOf<T>, 
+			currency_id, 
+			quote_price: PriceUnit
+		);
+		let supply_change = Self::calculate_supply_change(price);
+		let serp_quoted_price = Self::quote_serp_price(price);
+		let settpay_ratio = &T::GetSettPayRatio; // 75% for SettPay. It was statically typed, now moved to runtime and can be set there.
+		let serper_ratio = &T::GetSerperRatio; // 25% for Serpers. It was statically typed, now moved to runtime and can be set there.
+		let supply_ratio = Fix::from_num(expand_by) / Fix::from_num(100); // Percentage, meaning 1%.
+		let settpay_distro = supply_ratio.saturating_mul_int(settpay_ratio as u128).to_num::<u64>(); // 75% distro for SettPay.
+		let serper_distro = supply_ratio.saturating_mul_int(serper_ratio as u128).to_num::<u64>(); // 25% distro for Serpers.
+		let pay_by_quoted = serper_distro.saturating_div_int(serp_quoted_price as u128).to_num::<u64>();
+		if currency_id == native_asset_id::get() {
+			debug::warn!("Cannot expand supply for NativeCurrency: {}", currency_id);
+			return Err(http::Error::Unknown);
+		} else {
+			T::SettCurrency::set_free_balance(currency_id, settpay, account.free + settpay_distro);
+			T::SettCurrency::set_reserved_balance(currency_id, serper, account.reserved + serper_distro);
+			T::Stp258Native::set_reserved_balance(serper, account.reserved - pay_by_quoted);
+		}
+		// safe to do this late because of the overflow test in the second line of the function
+		native::info!("expanded supply by serping settcurrency: {} with amount: {}", currency_id, expand_by);
+		T::SettCurrency::<TotalIssuance<T>>::mutate(currency_id, |v| *v += expand_by);
+		native::info!("burned native asset: {} by serping settcurrency: {} with amount: {} {}", native_asset_id, currency_id, native_asset_id, pay_by_quoted);
+		T::Stp258Native::<TotalIssuance<T>>::mutate(native_asset_id, |v| *v -= pay_by_quoted);
+		<Price>::put(currency_id, new_price);
+		native::info!("The new price of: {} is : {}", currency_id, new_price);
+		Self::deposit_event(Event::SerpedUpSupply(currency_id, expand_by));
+		Self::deposit_event(Event::NewPrice(currency_id, serp_quoted_price));
+		Ok(())
+	}
+
+	/// Called when `contract_supply` is received from the SERP.
+	/// Implementation should `deposit` the `base_currency_id` (The Native Currency) 
+	/// of `amount` to `serpup_to`, then `amount` will be slashed from `serpup_from` 
+	/// and update `new_supply`.
+	#[pallet::weight(0)]
+	fn contract_supply(
+		currency_id: Self::CurrencyId,
+		contract_by: Self::Balance
+	) -> DispatchResult{
+		let supply = T::SettCurrency::total_issuance();
+		// Checking whether the supply will overflow.
+		supply
+			.checked_sub(contract_by)
+			.ok_or(Error::<T>::SupplyUnderflow)?;
+		// ↑ verify ↑
+		let serper = &T::GetSerperAcc;
+		let settpay: &T::GetSettPayAcc;
+		let base_currency_id = currency_id;
+		let quote_currency_id = native_asset_id;
+		let price = Self::get_relative_price(
+			currency_id, 
 			quote_price: PriceUnit,
-		) -> DispatchResult {
-			type Fix = FixedU128<U64>;
-			let amount_of_quote_to_buy_base = Fix::from_num(base_price) / Fix::from_num(quote_price);
-			let amount_of_base_to_buy_quote = Fix::from_num(quote_price) / Fix::from_num(base_price);
-			Price::put(base_currency_id, amount_of_quote_to_buy_base); /// the amount of quote currency that can buy base currency.
-			native::info!(
-				"The price of: {} for: {}  is {}, therefore {} {} can buy {} {}", 
-				base_currency_id, 
-				quote_currency_id, 
-				amount_of_quote_to_buy_base, 
-				quote_currency_id, 
-				amount_of_quote_to_buy_base, 
-				base_currency_id, 
-				amount_of_base_to_buy_quote,
-			);
-			Ok(())
+			native_asset_id,
+			base_price: PriceUnit, 
+		);
+		let supply_change = contract_by;
+		let serp_quoted_price = Self::quote_serp_price(price);
+		let new_price = serp_quoted_price;
+		let pay_by_quoted = serp_quoted_price.saturating_mul_int(supply_change as u128).to_num::<u64>();
+		if currency_id == T::GetStp258NativeId::get() {
+			debug::warn!("Cannot expand supply for NativeCurrency: {}", currency_id);
+			return Err(http::Error::Unknown);
+		} else {
+			T::SettCurrency::set_reserved_balance(currency_id, serper, account.reserved - contract_by);
+			T::Stp258Native::set_reserved_balance(serper, account.reserved + pay_by_quoted);
 		}
-
-		/// Quote the amount of currency price quoted as serping fee (serp quoting) for Serpers, 
-		/// the Serp Quote is `price/base_unit = fraction`, `fraction - 1 = fractioned`, `fractioned * serp_quote_multiple = quotation`,
-		/// `quotation + fraction = quoted` and `quoted` is the price the SERP will pay for serping in full including the serp_quote,
-		///  the fraction is same as `(market_price + (mint_rate * 2))` - where `market-price = price/base_unit`, 
-		/// `mint_rate = serp_quote_multiple`, and with `(price/base_unit) - 1 = price_change`.
-		///
-		/// Calculate the amount of currency price for SerpMarket's SerpQuote from a fraction given as `numerator` and `denominator`.
-		#[pallet::weight(0)]
-		fn quote_serp_price(price: PriceUnit) -> PriceUnit {
-			type Fix = FixedU128<U64>;
-			let base_unit = T::GetBaseUnit;
-			/// The `serp_quote_multiple` was in place as "[`let serp_quote_multiple = 2;` // (mint_rate * 2) for Serp Quote]".
-			let serp_quote_multiple = T::GetSerpQuoteMultiple;
-			let fraction = Fix::from_num(price) / Fix::from_num(base_unit);
-			let fractioned = Fix::from_num(fraction) - Fix::from_num(1);
-			let quotation = fractioned.saturating_mul_int(serp_quote_multiple as u128).to_num::<u64>();
-			quoted = Fix::from_num(fraction) + Fix::from_num(quotation);
-			Ok(())
-		}
-
-		/// Calculate the amount of supply change from a fraction given as `numerator` and `denominator`.
-		#[pallet::weight(0)]
-		fn calculate_supply_change(new_price: PriceUnit) -> PriceUnit {
-			type Fix = FixedU128<U64>;
-			let base_unit = T::GetBaseUnit; 
-			let supply = T::SettCurrency::total_issuance();
-			let fraction = Fix::from_num(new_price) / Fix::from_num(base_unit) - Fix::from_num(1);
-			fraction.saturating_mul_int(supply as u128).to_num::<u64>();
-			Ok(())
-		}
-
-		/// Called when `expand_supply` is received from the SERP.
-		/// Implementation should `deposit` the `amount` to `serpup_to`, 
-		/// then `amount` will be slashed from `serpup_from` and update
-		/// `new_supply`.
-		#[pallet::weight(0)]
-		fn expand_supply(
-			currency_id: Self::CurrencyId, 
-			expand_by: Self::Balance,
-		) -> DispatchResult{
-			let supply = T::SettCurrency::total_issuance();
-			// Checking whether the supply will overflow.
-			supply
-				.checked_add(expand_by)
-				.ok_or(Error::<T>::SupplyOverflow)?;
-			// ↑ verify ↑
-			let native_asset_id = T::GetStp258NativeId;
-			let serper = &T::GetSerperAcc; 
-			let settpay = &T::GetSettPayAcc;
-			let base_currency_id = currency_id;
-			let quote_currency_id = native_asset_id;
-			let price = Self::get_relative_price(
-				native_asset_id,
-				base_price: PriceUnit, 
-				currency_id, 
-				quote_price: PriceUnit
-			);
-			let supply_change = Self::calculate_supply_change(price);
-			let serp_quoted_price = Self::quote_serp_price(price);
-			let settpay_ratio = &T::GetSettPayRatio; // 75% for SettPay. It was statically typed, now moved to runtime and can be set there.
-			let serper_ratio = &T::GetSerperRatio; // 25% for Serpers. It was statically typed, now moved to runtime and can be set there.
-			let supply_ratio = Fix::from_num(expand_by) / Fix::from_num(100); // Percentage, meaning 1%.
-			let settpay_distro = supply_ratio.saturating_mul_int(settpay_ratio as u128).to_num::<u64>(); // 75% distro for SettPay.
-			let serper_distro = supply_ratio.saturating_mul_int(serper_ratio as u128).to_num::<u64>(); // 25% distro for Serpers.
-			let pay_by_quoted = serper_distro.saturating_div_int(serp_quoted_price as u128).to_num::<u64>();
-			if currency_id == native_asset_id::get() {
-				debug::warn!("Cannot expand supply for NativeCurrency: {}", currency_id);
-				return Err(http::Error::Unknown);
-			} else {
-				T::SettCurrency::set_free_balance(currency_id, settpay, account.free + settpay_distro);
-				T::SettCurrency::set_reserved_balance(currency_id, serper, account.reserved + serper_distro);
-				T::Stp258Native::set_reserved_balance(serper, account.reserved - pay_by_quoted);
-			}
-			// safe to do this late because of the overflow test in the second line of the function
-			native::info!("expanded supply by serping settcurrency: {} with amount: {}", currency_id, expand_by);
-			T::SettCurrency::<TotalIssuance<T>>::mutate(currency_id, |v| *v += expand_by);
-			native::info!("burned native asset: {} by serping settcurrency: {} with amount: {} {}", native_asset_id, currency_id, native_asset_id, pay_by_quoted);
-			T::Stp258Native::<TotalIssuance<T>>::mutate(native_asset_id, |v| *v -= pay_by_quoted);
-			<Price>::put(currency_id, new_price);
-			native::info!("The new price of: {} is : {}", currency_id, new_price);
-			Self::deposit_event(Event::SerpedUpSupply(currency_id, expand_by));
-			Self::deposit_event(Event::NewPrice(currency_id, serp_quoted_price));
-			Ok(())
-		}
-
-		/// Called when `contract_supply` is received from the SERP.
-		/// Implementation should `deposit` the `base_currency_id` (The Native Currency) 
-		/// of `amount` to `serpup_to`, then `amount` will be slashed from `serpup_from` 
-		/// and update `new_supply`.
-		#[pallet::weight(0)]
-		fn contract_supply(
-			currency_id: Self::CurrencyId,
-			contract_by: Self::Balance
-		) -> DispatchResult{
-			let supply = T::SettCurrency::total_issuance();
-			// Checking whether the supply will overflow.
-			supply
-				.checked_sub(contract_by)
-				.ok_or(Error::<T>::SupplyUnderflow)?;
-			// ↑ verify ↑
-			let serper = &T::GetSerperAcc;
-			let settpay: &T::GetSettPayAcc;
-			let base_currency_id = currency_id;
-			let quote_currency_id = native_asset_id;
-			let price = Self::get_relative_price(
-				currency_id, 
-				quote_price: PriceUnit,
-				native_asset_id,
-				base_price: PriceUnit, 
-			);
-			let supply_change = contract_by;
-			let serp_quoted_price = Self::quote_serp_price(price);
-			let new_price = serp_quoted_price;
-			let pay_by_quoted = serp_quoted_price.saturating_mul_int(supply_change as u128).to_num::<u64>();
-			if currency_id == T::GetStp258NativeId::get() {
-				debug::warn!("Cannot expand supply for NativeCurrency: {}", currency_id);
-				return Err(http::Error::Unknown);
-			} else {
-				T::SettCurrency::set_reserved_balance(currency_id, serper, account.reserved - contract_by);
-				T::Stp258Native::set_reserved_balance(serper, account.reserved + pay_by_quoted);
-			}
-			// safe to do this late because of the overflow test in the second line of the function
-			native::info!("contracted supply by serping settcurrency: {} with amount: {}", currency_id, contract_by);
-			T::SettCurrency::<TotalIssuance<T>>::mutate(currency_id, |v| *v -= contract_by);
-			native::info!("mined native asset : {} by serping settcurrency: {} with amount: {} {}", native_asset_id, currency_id, native_asset_id, pay_by_quoted);
-			T::Stp258Native::<TotalIssuance<T>>::mutate(native_asset_id, |v| *v += pay_by_quoted);
-			<Price>::put(currency_id, new_price);
-			native::info!("The price of: {} is: {} in its peg currency.", currency_id, new_price);
-			Self::deposit_event(Event::SerpedDownSupply(currency_id, who.clone(), contract_by));
-			Self::deposit_event(Event::NewPrice(currency_id, serp_quoted_price));
-			Ok(())
-		}
+		// safe to do this late because of the overflow test in the second line of the function
+		native::info!("contracted supply by serping settcurrency: {} with amount: {}", currency_id, contract_by);
+		T::SettCurrency::<TotalIssuance<T>>::mutate(currency_id, |v| *v -= contract_by);
+		native::info!("mined native asset : {} by serping settcurrency: {} with amount: {} {}", native_asset_id, currency_id, native_asset_id, pay_by_quoted);
+		T::Stp258Native::<TotalIssuance<T>>::mutate(native_asset_id, |v| *v += pay_by_quoted);
+		<Price>::put(currency_id, new_price);
+		native::info!("The price of: {} is: {} in its peg currency.", currency_id, new_price);
+		Self::deposit_event(Event::SerpedDownSupply(currency_id, who.clone(), contract_by));
+		Self::deposit_event(Event::NewPrice(currency_id, serp_quoted_price));
+		Ok(())
+	}
 }
